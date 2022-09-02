45a3b7cc719182cb4d8b9f1e67f407c3bf9a2201
Lopez Anthony <70229078+Antho-Lopez@users.noreply.github.com>
2022-09-02 02:22:45 +0200

    Add Heroku
    Add heroku Procfile

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -19,7 +19,6 @@ class SessionController extends Controller
         foreach($all_sessions as $one_session){
             foreach($one_session->repetitions as $repetition){
                 if(array_key_exists($count_key, $kilos_lifted)){
-
                     $kilos_lifted[$count_key][0] += round($repetition->nb_repetitions * $repetition->kilos, 2);
                 } else {
                     $kilos_lifted[$count_key][0] = round($repetition->nb_repetitions * $repetition->kilos, 2);
