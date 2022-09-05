1fd795915a6a2e0b8e5854c3acca5664a1612de0
Lopez Anthony <70229078+Antho-Lopez@users.noreply.github.com>
2022-09-06 01:36:55 +0200

    Update 2022_09_05_233058_add_age_to_users_table.php

--- database/migrations/2022_09_05_233058_add_age_to_users_table.php
+++ database/migrations/2022_09_05_233058_add_age_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('age');
+            $table->integer('age')->nullable();
         });
     }
 
