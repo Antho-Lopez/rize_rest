045dd1a3b03219e686979f9cb93eaaa6b83dbc4e
Lopez Anthony <70229078+Antho-Lopez@users.noreply.github.com>
2022-09-07 06:06:47 +0200

    Update SleepController.php

--- app/Http/Controllers/SleepController.php
+++ app/Http/Controllers/SleepController.php
@@ -22,41 +22,42 @@ class SleepController extends Controller
             array_push($get_days, $days->day_id);
         }
 
-        // $data = $request->validate([
-        //     'day_id' => 'array',
-        //     'go_to_sleep' => 'array',
-        //     'waking_up' => 'array',
-        // ]);
+        $data = $request->validate([
+            'day_id' => 'array',
+            'go_to_sleep' => '',
+            'waking_up' => '',
+        ]);
 
-        // $days_id = $request->input('day_id');
-        $days_id = [1, 4 , 6];
+        $days_id = $request->input('day_id');
+        // $days_id = [1, 2, 3];
+
         $count = 0;
 
-        $data['day_id'] = [1, 4, 6];
-        $data['go_to_sleep'] = ['21:00:00', '22:00:00', '23:00:00'];
-        $data['waking_up'] = ['11:00:00', '12:00:00', '13:00:00'];
+        // $data['day_id'] = [1, 2, 3];
+        // $data['go_to_sleep'] = '19:00:00';
+        // $data['waking_up'] = '09:00:00';
 
         foreach($days_id as $day_id){
 
             if(in_array($day_id, $get_days)){
 
-                dump('update', $data['go_to_sleep'][$count], $data['waking_up'][$count], $data['day_id'][$count]);
+                dump('update', $data['go_to_sleep'], $data['waking_up'], $data['day_id'][$count]);
 
                 Sleep::where('user_id', $user_id)->where('day_id', $data['day_id'][$count])->update([
-                    'go_to_sleep' => $data['go_to_sleep'][$count],
-                    'waking_up' => $data['waking_up'][$count],
+                    'go_to_sleep' => $data['go_to_sleep'],
+                    'waking_up' => $data['waking_up'],
                 ]);
                 $count++;
 
             } else {
 
-                dump('create' , $data['go_to_sleep'][$count], $data['waking_up'][$count], $data['day_id'][$count]);
+                dump('create' , $data['go_to_sleep'], $data['waking_up'], $data['day_id'][$count]);
 
                 Sleep::create([
                     'day_id' => $day_id,
                     'user_id' => $user_id,
-                    'go_to_sleep' => $data['go_to_sleep'][$count],
-                    'waking_up' => $data['waking_up'][$count],
+                    'go_to_sleep' => $data['go_to_sleep'],
+                    'waking_up' => $data['waking_up'],
                 ]);
                 $count++;
 
